3b697f154f2a7c6ee1ef7a6e055ce11e793160e1
Ron Dagdag <ron@spacee.com>
2020-10-19 11:26:28 -0500

    update to onnx runtime 1.5.2 add sample onnx runtime in node add sample onnx runtime in python

---
+++ onnx-node/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+async function start() {
+    console.log('Hello world');
+
+    // import the runtime
+
+    const ort = require('onnxruntime');
+
+
+    // loading the model
+
+    const session = await ort.InferenceSession.create('./model.onnx');
+
+
+    // prepare inputs. a tensor need its corresponding TypedArray as data
+
+    const yearsExperience = Float32Array.from([1.1]);
+
+    const salary = Float32Array.from([0]);
+
+    const tensorYrs = new ort.Tensor('float32', yearsExperience, [1, 1]);
+
+    const tensorSalary = new ort.Tensor('float32', salary, [1, 1]);
+
+
+    // prepare feeds. use model input names as keys.
+
+    const feeds = { yearsExperience: tensorYrs, salary: tensorSalary };
+
+
+    // feed inputs and run
+
+    const results = await session.run(feeds);
+
+
+    // read from results
+
+    const predictedSalary = results["Score.output"].data[0];
+
+    console.log(`Predicted Salary: ${predictedSalary}`);
+
+}
+
+start();
